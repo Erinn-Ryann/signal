8e8cd21dc37a020f3343e1db9a920ccd64d757c9
Erinn-Ryann <mpaboterinnryann@gmail.com>
2026-04-22 10:26:45 +0200

    try

--- hooks/useColorScheme.web.ts
+++ hooks/useColorScheme.web.ts
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import { useColorScheme as useRNColorScheme } from 'react-native';
+import { useEffect, useState } from "react";
+import { useColorScheme as useRNColorScheme } from "react-native";
 
 /**
  * To support static rendering, this value needs to be re-calculated on the client side for web
@@ -10,6 +10,7 @@ export function useColorScheme() {
   useEffect(() => {
     setHasHydrated(true);
   }, []);
+  //test
 
   const colorScheme = useRNColorScheme();
 
@@ -17,5 +18,5 @@ export function useColorScheme() {
     return colorScheme;
   }
 
-  return 'light';
+  return "light";
 }
